d5eb57320b9340b0a00a737d68df481406ed8358
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2004-04-28 15:38:26 +0000

    Everything is different ... now that's good !

--- Pop/Individual.cpp
+++ Pop/Individual.cpp
@@ -1,23 +1,31 @@
 /*
  * File Individual.cpp
  * Author : Sylvain Gaillard <yragael2001@yahoo.fr>
- * Last modification : Tuesday April 27 2004
+ * Last modification : Wednesday April 28 2004
  */
 
 #include "Individual.h"
 
 //** Class constructor: *******************************************************/
 Individual::Individual() {
+	this->_sex = 0;
 	this->_date = NULL;
 	this->_coord = NULL;
 	this->_locality = NULL;
 }
 
+Individual::Individual(const Date & date, const Coord<double> & coord, const Locality<double> & locality, const unsigned short sex) {
+	this->_sex = sex;
+	this->_date = new Date(date);
+	this->_coord = new Coord<double>(coord);
+	this->_locality = new Locality<double>(locality);
+}
+
 Individual::Individual(const Individual &ind) {
 	this->_sex = ind.getSex();
-	this->_date = ind.getDate();
-	this->_coord = ind.getCoord();
-	this->_locality = ind.getLocality();
+	this->_date = ind.hasDate() ? new Date(* ind.getDate()) : NULL;
+	this->_coord = ind.hasCoord() ? new Coord<double>(* ind.getCoord()) : NULL;
+	this->_locality = ind.hasLocality() ? new Locality<double>(* ind.getLocality()) : NULL;
 }
 
 //** Class destructor: *******************************************************/
@@ -34,12 +42,13 @@ Clonable * Individual::clone() const {
 
 Individual & Individual::operator= (const Individual & ind) {
 	this->_sex = ind.getSex();
-	this->_date = ind.getDate();
-	this->_coord = ind.getCoord();
-	this->_locality = ind.getLocality();
+	this->_date = ind.hasDate() ? new Date(* ind.getDate()) : NULL;
+	this->_coord = ind.hasCoord() ? new Coord<double>(* ind.getCoord()) : NULL;
+	this->_locality = ind.hasLocality() ? new Locality<double>(* ind.getLocality()) : NULL;
 	return * this;
 }
 
+// Sex
 void Individual::setSex(const unsigned short sex) {
 	_sex = sex;
 }
@@ -48,9 +57,10 @@ unsigned short Individual::getSex() const {
 	return _sex;
 }
 
+// Date
 void Individual::setDate(const Date & date) {
-	if (_date == NULL) {
-		_date = newDate(date);
+	if (!hasDate()) {
+		_date = new Date(date);
 	}
 	else if (* _date != date) {
 		delete _date;
@@ -58,15 +68,20 @@ void Individual::setDate(const Date & date) {
 	}
 }
 
-Date Individual::getDate() const throw(NullPointerException) {
-	if (_date != NULL)
-		return new date(_date);
+Date * Individual::getDate() const throw(NullPointerException) {
+	if (hasDate())
+		return new Date(* _date);
 	else
-		throw(NullPointerException("Date not set."));
+		throw(NullPointerException("Individual::getDate: no date associated to this individual."));
 }
 
+bool Individual::hasDate() const {
+	return _date != NULL;
+}
+
+// Coord
 void Individual::setCoord(const Coord<double> & coord) {
-	if (_coord == NULL) {
+	if (!hasCoord()) {
 		_coord = new Coord<double>(coord);
 	}
 	else if	(* _coord != coord) {
@@ -76,7 +91,7 @@ void Individual::setCoord(const Coord<double> & coord) {
 }
 
 void Individual::setCoord(const double x, const double y) {
-	if (_coord == NULL) {
+	if (!hasCoord()) {
 		_coord = new Coord<double>(x, y);
 	}
 	else if (this->getX() != x || this->getY() != y) {
@@ -85,43 +100,48 @@ void Individual::setCoord(const double x, const double y) {
 	}
 }
 
-Coord<double> Individual::getCoord() const throw(NullPointerException) {
-	if (_coord != NULL)
-		return new Coord<double>(_coord);
+Coord<double> * Individual::getCoord() const throw(NullPointerException) {
+	if (hasCoord())
+		return new Coord<double>(* _coord);
 	else
-		throw(NullPointerException("Coord not set."));
+		throw(NullPointerException("Individual::getCoord: no coord associated to this individual."));
+}
+
+bool Individual::hasCoord() const {
+	return _coord != NULL;
 }
 
 void Individual::setX(const double x) throw(NullPointerException) {
-	if (_coord != NULL)
+	if (hasCoord())
 		_coord->setX(x);
 	else
-		throw(NullPointerException("Coord not initialized."));
+		throw(NullPointerException("Individual::setX: no coord associated to this individual."));
 }
 
 void Individual::setY(const double y) throw(NullPointerException) {
-	if (_coord != NULL)
+	if (hasCoord())
 		_coord->setY(y);
 	else
-		throw(NullPointerException("Coord not initialized."));
+		throw(NullPointerException("Individual::setY: no coord associated to this individual."));
 }
 
 double Individual::getX() const throw(NullPointerException) {
-	if (_coord != NULL)
+	if (hasCoord())
 		return _coord->getX();
 	else
-		throw(NullPointerException("X not set."));
+		throw(NullPointerException("Individual::getX: no coord associated to this individual."));
 }
 
 double Individual::getY() const throw(NullPointerException) {
-	if (_coord != NULL)
+	if (hasCoord())
 		return _coord->getY();
 	else
-		throw(NullPointerException("Y not set."));
+		throw(NullPointerException("Individual::getY: no coord associated to this individual."));
 }
 
+// Locality
 void Individual::setLocality(const Locality<double> & locality) {
-	if (_locality == NULL)
+	if (!hasLocality())
 		_locality = new Locality<double>(locality);
 	else if (* _locality != locality) {
 		delete _locality;
@@ -130,16 +150,16 @@ void Individual::setLocality(const Locality<double> & locality) {
 }
 
 void Individual::setLocality(const string name, const Coord<double> & coord) {
-	if (_locality == NULL)
+	if (!hasLocality())
 		_locality = new Locality<double>(name, coord);
-	else if (_locality->getName() != name || _locality->getCoord() != coord) {
+	else if (_locality->getName() != name || * (_locality->getCoord()) != coord) {
 		delete _locality;
 		_locality = new Locality<double>(name, coord);
 	}
 }
 
 void Individual::setLocality(const string name, const double x, const double y) {
-	if (_locality == NULL)
+	if (!hasLocality())
 		_locality = new Locality<double>(name, x, y);
 	else if (_locality->getName() != name || _locality->getX() != x || _locality->getY() != y) {
 		delete _locality;
@@ -147,9 +167,13 @@ void Individual::setLocality(const string name, const double x, const double y) 
 	}
 }
 
-Locality<double> Individual::getLocality() const throw(NullPointerException) {
-	if (_locality != NULL)
-		return new Locality<double>(_locality);
+Locality<double> * Individual::getLocality() const throw(NullPointerException) {
+	if (hasLocality())
+		return new Locality<double>(* _locality);
 	else
-		throw(NullPointerException("Locality  not set."));
+		throw(NullPointerException("Individual::getLocality: no locality associated to this individual."));
+}
+
+bool Individual::hasLocality() const {
+	return _locality != NULL;
 }
--- Pop/Individual.h
+++ Pop/Individual.h
@@ -1,7 +1,7 @@
 /*
  * File Individual.h
  * Author : Sylvain Gaillard <yragael2001@yahoo.fr>
- * Last modification : Tuesday April 27 2004
+ * Last modification : Wednesday April 28 2004
  */
 
 // Secured inclusion of header's file
@@ -30,6 +30,16 @@ class Individual : public Clonable {
 		 */
 		Individual();
 
+		/**
+		 * @brief Build a new Individual with parameters.
+		 *
+		 * @param date The date of the Individual as a Date object.
+		 * @param coord The coordinates of the Individual as a Coord object.
+		 * @param locality The locality of the Individual as a Locality object.
+		 * @param sex The sex of the Individual as an unsigned short.
+		 */
+		Individual(const Date & date, const Coord<double> & coord, const Locality<double> & locality, const unsigned short sex);
+		
 		/**
 		 * @brief The Individual copy constructor.
 		 */
@@ -78,9 +88,14 @@ class Individual : public Clonable {
 		/**
 		 * @brief Get the date of the Individual.
 		 *
-		 * @return The date of the Individual as a Date.
+		 * @return A pointer toward a Date object if the Individual has a date. Otherwise throw a NullPointerException.
 		 */
-		Date getDate() const throw(NullPointerException);
+		Date * getDate() const throw(NullPointerException);
+
+		/**
+		 * @brief Tell if this Individual has a date.
+		 */
+		bool hasDate() const;
 
 		/**
 		 * @brief Set the coodinates of the Individual.
@@ -100,52 +115,83 @@ class Individual : public Clonable {
 		/**
 		 * @brief Get the coordinates of the Induvidual.
 		 *
-		 * @return The coordinates of the Individual as a Coord object.
+		 * @return A pointer toward a Coord object if the Individual has coordinates. Otherwise throw a NullPointerException.
 		 */
-		Coord<double> getCoord() const throw(NullPointerException);
+		Coord<double> * getCoord() const throw(NullPointerException);
+
+		/**
+		 * @brief Tell if this Individual has coordinates.
+		 */
+		bool hasCoord() const;
 
 		/**
 		 * @brief Set the X coordinate of the Individual.
+		 *
+		 * @param x The X coordinate as a double.
+		 *
+		 * Set the X coordinate if the Individual has coordinates. Otherwise throw a NullPointerException.
 		 */
 		void setX(const double x) throw(NullPointerException);
 
 		/**
 		 * @brief Set the Y coordinate of th Individual.
+		 *
+		 * @param The Y coordinate as a double.
+		 *
+		 * Set the Y coordinate if the Individual has coordinates. Otherwise throw a NullPointerException.
 		 */
 		void setY(const double y) throw(NullPointerException);
 
 		/**
 		 * @brief Get the X coordinate of the Individual.
+		 *
+		 * @return The X coordinate as a double if the Individual has coordinates. Otherwise throw a NullPointerException.
 		 */
 		double getX() const throw(NullPointerException);
 
 		/**
 		 * @brief Get the Y coordinate of the Individual.
+		 *
+		 * @return The Y coordinate as a double if the Individual has coordinates. Otherwise throw a NullPointerException.
 		 */
 		double getY() const throw(NullPointerException);
 
 		/**
 		 * @brief Set the locality of the Individual.
+		 *
+		 * @param locality A Locality object.
 		 */
 		void setLocality(const Locality<double> & locality);
 		
 		/**
 		 * @brief Set the locality of the Individual.
+		 *
+		 * @param name The name of the locality as a string.
+		 * @param coord The coordinates of the locality as a Coord object.
 		 */
 		void setLocality(const string name, const Coord<double> & coord);
 
 		/**
 		 * @brief Set the locality of the Individual.
+		 *
+		 * @param name The name of the locality as a string.
+		 * @param x The X coordinate of the locality as a double.
+		 * @param y The Y coordinate of the locality as a double.
 		 */
 		void setLocality(const string name, const double x, const double y);
 
 		/**
 		 * @brief Get the locality of the Individual.
 		 *
-		 * @return The locality of the Individual as a Locality object.
+		 * @return A pointer toward a Locality object if the Individual has a locality. Otherwise throw a NullPointerException.
 		 */
-		Locality<double> getLocality() const throw(NullPointerException);
+		Locality<double> * getLocality() const throw(NullPointerException);
 
+		/**
+		 * @brief Tell if this Individual has a locality.
+		 */
+		bool hasLocality() const;
+		
 	protected:
 		unsigned short _sex;
 		Date * _date;
