f1566b97ceee40e2ca7743400d20a5109b2722de
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2004-06-21 12:47:05 +0000

    Old Individual ... no more in use ... but need to be there.

---
+++ Pop/MultiSeqIndividual.cpp
@@ -0,0 +1,332 @@
+/*
+ * File MultiSeqIndividual.cpp
+ * Author : Sylvain Gaillard <yragael2001@yahoo.fr>
+ * Last modification : Monday June 21 2004
+ */
+
+#include "MultiSeqIndividual.h"
+
+//** Class constructor: *******************************************************/
+MultiSeqIndividual::MultiSeqIndividual() {
+	_id = "";
+	_sex = 0;
+	_date = NULL;
+	_coord = NULL;
+	_locality = NULL;
+	_genotype = NULL;
+}
+
+MultiSeqIndividual::MultiSeqIndividual(const string & id) {
+	_id = id;
+	_sex = 0;
+	_date = NULL;
+	_coord = NULL;
+	_locality = NULL;
+	_genotype = NULL;
+}
+
+MultiSeqIndividual::MultiSeqIndividual(const string & id,
+                       const Date & date,
+                       const Coord<double> & coord,
+                       Locality<double> * locality,
+                       const unsigned short sex) {
+	_id = id;
+	_sex = sex;
+	_date = new Date(date);
+	_coord = new Coord<double>(coord);
+	_locality = locality;
+}
+
+MultiSeqIndividual::MultiSeqIndividual(const MultiSeqIndividual &ind) {
+	setId(ind.getId());
+	setSex(ind.getSex());
+	try {
+		setDate(* ind.getDate());
+	}
+	catch (NullPointerException) {
+		_date = NULL;
+	}
+	try {
+		setCoord(* ind.getCoord());
+	}
+	catch (NullPointerException) {
+		_coord = NULL;
+	}
+	try {
+		setLocality(ind.getLocality());
+	}
+	catch (NullPointerException) {
+		_locality = NULL;
+	}
+	if (ind.hasSequences()) {
+		vector<string> keys = ind.getSequencesKeys();
+		for (unsigned int i = 0 ; i < keys.size() ; i++)
+			_sequences[keys[i]] = new VectorSequenceContainer(* const_cast<const VectorSequenceContainer *>(ind.getVectorSequenceContainer(keys[i])));
+	}
+	this->_genotype = ind.hasGenotype() ? new Genotype(* ind.getGenotype()) : NULL;
+}
+
+//** Class destructor: *******************************************************/
+MultiSeqIndividual::~MultiSeqIndividual () {
+	delete this->_date;
+	delete this->_coord;
+}
+
+//** Other methodes: *********************************************************/
+Clonable * MultiSeqIndividual::clone() const {
+	return new MultiSeqIndividual(* this);
+}
+
+MultiSeqIndividual & MultiSeqIndividual::operator= (const MultiSeqIndividual & ind) {
+	setId(ind.getId());
+	setSex(ind.getSex());
+	try {
+		setDate(* ind.getDate());
+	}
+	catch (NullPointerException) {
+		_date = NULL;
+	}
+	try {
+		setCoord(* ind.getCoord());
+	}
+	catch (NullPointerException) {
+		_coord = NULL;
+	}
+	try {
+		setLocality(ind.getLocality());
+	}
+	catch (NullPointerException) {
+		_locality = NULL;
+	}
+	if (ind.hasSequences()) {
+		vector<string> keys = ind.getSequencesKeys();
+		for (unsigned int i = 0 ; i < keys.size() ; i++)
+			_sequences[keys[i]] = new VectorSequenceContainer(* const_cast<const VectorSequenceContainer *>(ind.getVectorSequenceContainer(keys[i])));
+	}
+	this->_genotype = ind.hasGenotype() ? new Genotype(* ind.getGenotype()) : NULL;
+	return * this;
+}
+
+// Id
+void MultiSeqIndividual::setId(const string id) {
+	_id = id;
+}
+
+string MultiSeqIndividual::getId() const {
+	return _id;
+}
+
+// Sex
+void MultiSeqIndividual::setSex(const unsigned short sex) {
+	_sex = sex;
+}
+
+unsigned short MultiSeqIndividual::getSex() const {
+	return _sex;
+}
+
+// Date
+void MultiSeqIndividual::setDate(const Date & date) {
+	if (!hasDate()) {
+		_date = new Date(date);
+	}
+	else if (* _date != date) {
+		delete _date;
+		_date = new Date(date);
+	}
+}
+
+const Date * MultiSeqIndividual::getDate() const throw (NullPointerException) {
+	if (hasDate())
+		return new Date(* _date);
+	else
+		throw(NullPointerException("MultiSeqIndividual::getDate: no date associated to this individual."));
+}
+
+bool MultiSeqIndividual::hasDate() const {
+	return _date != NULL;
+}
+
+// Coord
+void MultiSeqIndividual::setCoord(const Coord<double> & coord) {
+	if (!hasCoord()) {
+		_coord = new Coord<double>(coord);
+	}
+	else if	(* _coord != coord) {
+		delete _coord;
+		_coord = new Coord<double>(coord);
+	}
+}
+
+void MultiSeqIndividual::setCoord(const double x, const double y) {
+	if (!hasCoord()) {
+		_coord = new Coord<double>(x, y);
+	}
+	else if (this->getX() != x || this->getY() != y) {
+		delete _coord;
+		_coord = new Coord<double>(x, y);
+	}
+}
+
+const Coord<double> * MultiSeqIndividual::getCoord() const throw(NullPointerException) {
+	if (hasCoord())
+		return new Coord<double>(* _coord);
+	else
+		throw(NullPointerException("MultiSeqIndividual::getCoord: no coord associated to this individual."));
+}
+
+bool MultiSeqIndividual::hasCoord() const {
+	return _coord != NULL;
+}
+
+void MultiSeqIndividual::setX(const double x) throw(NullPointerException) {
+	if (hasCoord())
+		_coord->setX(x);
+	else
+		throw(NullPointerException("MultiSeqIndividual::setX: no coord associated to this individual."));
+}
+
+void MultiSeqIndividual::setY(const double y) throw(NullPointerException) {
+	if (hasCoord())
+		_coord->setY(y);
+	else
+		throw(NullPointerException("MultiSeqIndividual::setY: no coord associated to this individual."));
+}
+
+double MultiSeqIndividual::getX() const throw(NullPointerException) {
+	if (hasCoord())
+		return _coord->getX();
+	else
+		throw(NullPointerException("MultiSeqIndividual::getX: no coord associated to this individual."));
+}
+
+double MultiSeqIndividual::getY() const throw(NullPointerException) {
+	if (hasCoord())
+		return _coord->getY();
+	else
+		throw(NullPointerException("MultiSeqIndividual::getY: no coord associated to this individual."));
+}
+
+// Locality
+void MultiSeqIndividual::setLocality(const Locality<double> * locality) {
+	_locality = locality;
+}
+
+const Locality<double> * MultiSeqIndividual::getLocality() const  throw (NullPointerException) {
+	if (hasLocality())
+		return _locality;
+	else
+		throw(NullPointerException("MultiSeqIndividual::getLocality: no locality associated to this individual."));
+}
+
+bool MultiSeqIndividual::hasLocality() const {
+	return _locality != NULL;
+}
+
+// Sequences
+const VectorSequenceContainer * MultiSeqIndividual::getVectorSequenceContainer(const string & id) const throw (Exception) {
+	map<string, VectorSequenceContainer *>::const_iterator it;
+	it = _sequences.find(id);
+	// Test existence of id in the map.
+	if (it == _sequences.end()) {
+		string mes = "MultiSeqIndividual::getSequence: sequence set not found (" + id
+			+ ").";
+		throw(Exception(mes));
+	}
+	return const_cast<const VectorSequenceContainer *>(it->second);
+}
+
+void MultiSeqIndividual::addSequence(const string & id, const Sequence & sequence)
+throw (Exception) {
+	try {
+		_sequences[id]->addSequence(sequence);
+	}
+	catch (AlphabetMismatchException & ame)
+	{
+		throw(AlphabetMismatchException("MultiSeqIndividual::addSequence: alphabets don't match.", ame.getAlphabets()[0], ame.getAlphabets()[1]));
+	}
+	catch (Exception & e)
+	{
+		throw(BadIdentifierException("MultiSeqIndividual::addSequence: sequence's name already in use.", sequence.getName()));
+	}
+}
+
+const Sequence * MultiSeqIndividual::getSequence(const string & id, const string & name)
+const throw(Exception){
+	map<string, VectorSequenceContainer *>::const_iterator it;
+	it = _sequences.find(id);
+	// Test existence of id in the map.
+	if (it == _sequences.end()) {
+		string mes = "MultiSeqIndividual::getSequence: sequence set not found (" + id
+			+ ").";
+		throw(Exception(mes));
+	}
+	try {
+		return const_cast<const VectorSequenceContainer *>(it->second)->getSequence(name);
+	}
+	catch (SequenceNotFoundException & snfe) {
+		throw(snfe);
+	}
+}
+
+const Sequence * MultiSeqIndividual::getSequence(const string & id, unsigned int i)
+const throw(Exception) {
+	map<string, VectorSequenceContainer *>::const_iterator it;
+	it = _sequences.find(id);
+	// Test existence of id in the map.
+	if (it == _sequences.end()) {
+		string mes = "MultiSeqIndividual::getSequence: sequence set not found (" + id
+			+ ").";
+		throw(Exception(mes));
+	}
+	try {
+		return const_cast<const VectorSequenceContainer *>(it->second)->getSequence(i);
+	}
+	catch (IndexOutOfBoundsException & ioobe) {
+		throw(ioobe);
+	}
+}
+
+vector<string> MultiSeqIndividual::getSequencesKeys() const {
+	vector<string> keys;
+	map<string, VectorSequenceContainer *>::const_iterator it;
+	for (it = _sequences.begin() ; it != _sequences.end() ; it++)
+		keys.push_back(it->first);
+	return keys;
+}
+
+bool MultiSeqIndividual::hasSequences() const {
+	return _sequences.size() != 0;
+}
+
+unsigned int MultiSeqIndividual::getNumberOfSequenceSet() const {
+	return _sequences.size();
+}
+
+unsigned int MultiSeqIndividual::getNumberOfSequences(const string & id) const
+	throw (Exception) {
+	map<string, VectorSequenceContainer *>::const_iterator it;
+	it = _sequences.find(id);
+	// Test existence of id in the map.
+	if (it == _sequences.end()) {
+		string mes = "MultiSeqIndividual::getSequence: sequence set not found (" + id
+			+ ").";
+		throw(Exception(mes));
+	}
+	
+	return const_cast<const VectorSequenceContainer *>(it->second)->getNumberOfSequences();
+}
+
+// Genotype
+
+void MultiSeqIndividual::addGenotype(const Genotype & genotype) {
+	_genotype = new Genotype(genotype);
+}
+
+const Genotype * MultiSeqIndividual::getGenotype() const throw (NullPointerException) {
+		return _genotype;
+}
+
+bool MultiSeqIndividual::hasGenotype() const {
+	return _genotype != NULL;
+}
---
+++ Pop/MultiSeqIndividual.h
@@ -0,0 +1,334 @@
+/*
+ * File MultiSeqIndividual.h
+ * Author : Sylvain Gaillard <yragael2001@yahoo.fr>
+ * Last modification : Monday June 21 2004
+ */
+
+// Secured inclusion of header's file
+#ifndef _MULTISEQINDIVIDUAL_H_
+#define _MULTISEQINDIVIDUAL_H_
+
+// From STL
+#include <map>
+#include <vector>
+
+// From Utils
+#include <Utils/Clonable.h>
+#include <Utils/Exceptions.h>
+
+// From SeqLib
+#include <Seq/Sequence.h>
+#include <Seq/VectorSequenceContainer.h>
+#include <Seq/SequenceExceptions.h>
+
+// From PopLib
+#include "Locality.h"
+#include "Coord.h"
+#include "Date.h"
+#include "Genotype.h"
+#include "GeneralExceptions.h"
+
+/**
+ * @brief <center><b>*** UNUSED CLASS ***</b></center>The MultiSeqIndividual class.
+ *
+ * <center><b>*** UNUSED CLASS ***</b></center>
+ * This class is designed to store data on a single individual.
+ * This individual can store numerous sequence for each place. It was the first
+ * working implementation which manages sequences as a map of sequence container.
+ * We have replaced it with a simplest individual with only one sequence per
+ * locus.
+ */
+class MultiSeqIndividual : public Clonable {
+	public: // Constructors and destructor :
+		
+		/**
+		 * @brief Build a void new MultiSeqIndividual.
+		 */
+		MultiSeqIndividual();
+
+		/**
+		 * @brief Build a new MultiSeqIndividual with an identifier.
+		 */
+		MultiSeqIndividual(const string & id);
+
+		/**
+		 * @brief Build a new MultiSeqIndividual with parameters.
+		 *
+		 * @param id The id of the MultiSeqIndividual as a string.
+		 * @param date The date of the MultiSeqIndividual as a Date object.
+		 * @param coord The coordinates of the MultiSeqIndividual as a Coord object.
+		 * @param locality The locality of the MultiSeqIndividual as a pointer to a Locality
+		 * object.
+		 * @param sex The sex of the MultiSeqIndividual as an unsigned short.
+		 */
+		MultiSeqIndividual(const string & id,
+		           const Date & date,
+		           const Coord<double> & coord,
+		           Locality<double> * locality,
+		           const unsigned short sex);
+		
+		/**
+		 * @brief The MultiSeqIndividual copy constructor.
+		 */
+		MultiSeqIndividual(const MultiSeqIndividual &ind);
+
+		/**
+		 * @brief Destroy an MultiSeqIndividual.
+		 */
+		virtual ~MultiSeqIndividual();
+
+	public: // Methodes
+		
+		/**
+		 * @brief Implement the Clonable interface.
+		 */
+		Clonable * clone() const;
+
+		/**
+		 * @brief The MultiSeqIndividual copy operator.
+		 *
+		 * @return A ref toward the assigned MultiSeqIndividual.
+		 * Make a copy of each atribute of the MultiSeqIndividual.
+		 */
+		MultiSeqIndividual & operator= (const MultiSeqIndividual & ind);
+		
+		/**
+		 * @brief Set the id of the MultiSeqIndividual.
+		 *
+		 * @param id The id of the MultiSeqIndividual as a string.
+		 */
+		void setId(const string id);
+
+		/**
+		 * @brief Get the id of the MultiSeqIndividual.
+		 *
+		 * @return The id of the MultiSeqIndividual as a string.
+		 */
+		string getId() const;
+
+		/**
+		 * @brief Set the sex of the MultiSeqIndividual.
+		 *
+		 * @param sex An unsigned short coding for the sex.
+		 */
+		void setSex(const unsigned short sex);
+
+		/**
+		 * @brief Get the sex of the MultiSeqIndividual.
+		 *
+		 * @return The sex of the MultiSeqIndividual as an unsigned short.
+		 */
+		unsigned short getSex() const;
+
+		/**
+		 * @brief Set the date of the MultiSeqIndividual.
+		 *
+		 * @param date The date as a Date object.
+		 */
+		void setDate(const Date & date);
+
+		/**
+		 * @brief Get the date of the MultiSeqIndividual.
+		 *
+		 * @return A pointer toward a Date object if the MultiSeqIndividual has a date.
+		 * Otherwise throw a NullPointerException.
+		 */
+		const Date * getDate() const throw (NullPointerException);
+
+		/**
+		 * @brief Tell if this MultiSeqIndividual has a date.
+		 */
+		bool hasDate() const;
+
+		/**
+		 * @brief Set the coodinates of the MultiSeqIndividual.
+		 *
+		 * @param coord A Coord object.
+		 */
+		void setCoord(const Coord<double> & coord);
+
+		/**
+		 * @brief Set the coordinates of the MultiSeqIndividual.
+		 *
+		 * @param x The X coordinate as a double.
+		 * @param y The Y coordinate as a double.
+		 */
+		void setCoord(const double x, const double y);
+
+		/**
+		 * @brief Get the coordinates of the Induvidual.
+		 *
+		 * @return A pointer toward a Coord object if the MultiSeqIndividual has
+		 * coordinates. Otherwise throw a NullPointerException.
+		 */
+		const Coord<double> * getCoord() const throw(NullPointerException);
+
+		/**
+		 * @brief Tell if this MultiSeqIndividual has coordinates.
+		 */
+		bool hasCoord() const;
+
+		/**
+		 * @brief Set the X coordinate of the MultiSeqIndividual.
+		 *
+		 * @param x The X coordinate as a double.
+		 *
+		 * Set the X coordinate if the MultiSeqIndividual has coordinates.
+		 * Otherwise throw a NullPointerException.
+		 */
+		void setX(const double x) throw(NullPointerException);
+
+		/**
+		 * @brief Set the Y coordinate of th MultiSeqIndividual.
+		 *
+		 * @param y The Y coordinate as a double.
+		 *
+		 * Set the Y coordinate if the MultiSeqIndividual has coordinates.
+		 * Otherwise throw a NullPointerException.
+		 */
+		void setY(const double y) throw(NullPointerException);
+
+		/**
+		 * @brief Get the X coordinate of the MultiSeqIndividual.
+		 *
+		 * @return The X coordinate as a double if the MultiSeqIndividual has coordinates.
+		 * Otherwise throw a NullPointerException.
+		 */
+		double getX() const throw(NullPointerException);
+
+		/**
+		 * @brief Get the Y coordinate of the MultiSeqIndividual.
+		 *
+		 * @return The Y coordinate as a double if the MultiSeqIndividual has coordinates.
+		 * Otherwise throw a NullPointerException.
+		 */
+		double getY() const throw(NullPointerException);
+
+		/**
+		 * @brief Set the locality of the MultiSeqIndividual.
+		 *
+		 * @param locality A pointer to a Locality object.
+		 */
+		void setLocality(const Locality<double> * locality);
+
+		/**
+		 * @brief Get the locality of the MultiSeqIndividual.
+		 *
+		 * @return A pointer to the Locality of the MultiSeqIndividual.
+		 */
+		const Locality<double> * getLocality() const throw (NullPointerException);
+
+		/**
+		 * @brief Tell if this MultiSeqIndividual has a locality.
+		 */
+		bool hasLocality() const;
+
+		/**
+		 * @brief Get a pointer to the VectorSequenceContainer at a named locus.
+		 *
+		 * @param id The id of the sequence set (i.e. locus).
+		 */
+		const VectorSequenceContainer * getVectorSequenceContainer(const string & id) const
+			throw (Exception);
+
+		/**
+		 * @brief Add a sequence in a named sequence set.
+		 *
+		 * @param id The id of the sequence set.
+		 * @param sequence The sequence to add.
+		 * @throw AlphabetMismatchException if the sequence's alphabet doesn't match the container's alphabet.
+		 * @throw BadIdentifierException if sequence's name is already in use.
+		 */
+		void addSequence(const string & id, const Sequence & sequence)
+			throw (Exception);
+		
+		/**
+		 * @brief Get a named sequence from a named sequence set.
+		 *
+		 * @param id The id of the sequence set.
+		 * @param name The name of the sequence.
+		 *
+		 * @return A pointer to the sequence.
+		 */
+		const Sequence * getSequence(const string & id, const string & name)
+			const throw(Exception);
+
+		/**
+		 * @brief Get an indexed sequence from a named sequence set.
+		 *
+		 * @param id The id of the sequence set.
+		 * @param i The index of the sequence in the sequence set.
+		 *
+		 * @return A pointer tothe sequence.
+		 */
+		const Sequence * getSequence(const string & id, const unsigned int i)
+			const throw(Exception);
+		 
+		/**
+		 * @brief Get the sequence set ids.
+		 *
+		 * @return All the keys of the sequence sets in a vector.
+		 */
+		vector<string> getSequencesKeys() const;
+
+		/**
+		 * @brief Remove a named sequence from a named sequence set.
+		 *
+		 * @param id The id of the sequence set.
+		 * @param name The name of the sequence.
+		 *
+		 * @return A pointer to a copy of the removed sequence.
+		 */
+		Sequence * removeSequence(const string & id, const string & name);
+
+		/**
+		 * @brief Delete a named sequence from a named sequence set.
+		 *
+		 * @param id The id of the sequence set.
+		 * @param name The name of the sequence.
+		 */
+		void deleteSequence(const string & id, const string & name);
+
+		/**
+		 * @brief Tell if the MultiSeqIndividual has some sequences.
+		 */
+		bool hasSequences() const;
+
+		/**
+		 * @brief Count the number of sequece set.
+		 */
+		unsigned int getNumberOfSequenceSet() const;
+
+		/**
+		 * @brief Get the number of sequences in a sequence set.
+		 */
+		unsigned int getNumberOfSequences(const string & id) const
+			throw (Exception);
+
+		/**
+		 * @brief Add a genotype.
+		 *
+		 * @param genotype The Genotype to add.
+		 */
+		void addGenotype(const Genotype & genotype);
+
+		/**
+		 * @brief Get the genotype.
+		 */
+		const Genotype * getGenotype() const throw (NullPointerException);
+
+		/**
+		 * @brief Tell if the MultiSeqIndividual has a Genotype.
+		 */
+		bool hasGenotype() const;
+	
+	protected:
+		string _id;
+		unsigned short _sex;
+		Date * _date;
+		Coord<double> * _coord;
+		const Locality<double> * _locality;
+		map<string,VectorSequenceContainer *> _sequences;
+		Genotype * _genotype;
+};
+#endif // _MULTISEQINDIVIDUAL_H_
