448a86b22d9a7625222f45e2ee3763db61e14333
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2004-07-28 09:32:21 +0000

    Rewrited everything and added a method to extract sequences from a specified group into a new PolymorphismSequenceContainer.

--- Pop/PolymorphismSequenceContainerTools.cpp
+++ Pop/PolymorphismSequenceContainerTools.cpp
@@ -1,9 +1,9 @@
-//
-// File: PolymorphismSequenceContainerTools.cpp
-// Authors: bazin <bazin@univ-montp2.fr>
-//          Sylvain Gaillard <yragael2001@yahoo.fr>
-// Last modification : Tuesday July 27 2004
-//
+/*
+ * File: PolymorphismSequenceContainerTools.cpp
+ * Authors: bazin <bazin@univ-montp2.fr>
+ *          Sylvain Gaillard <yragael2001@yahoo.fr>
+ * Last modification : Wednesday July 28 2004
+ */
 
 // from PolyLib
 #include "PolymorphismSequenceContainerTools.h"
@@ -13,109 +13,131 @@ using namespace std;
 PolymorphismSequenceContainerTools::~PolymorphismSequenceContainerTools() {}
 
 // Read a mase+ format
-PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::read(const string & path, const Alphabet *
-alpha) throw (Exception) {
-	try {
+PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::read(const string & path, const Alphabet * alpha) throw (Exception) {
 	Mase ms;
-	string clef;
+	string key;
 	unsigned int n;
-	const VectorSequenceContainer *seqc = ms.read( path, alpha );
-	VectorSiteContainer *sitec = new VectorSiteContainer( *seqc );
-	PolymorphismSequenceContainer *psc = new PolymorphismSequenceContainer( *sitec );
-	Comments maseFileHeader = sitec -> getGeneralComments();
-	map< string, unsigned int > groupMap = MaseTools::getAvailableSequenceSelections(maseFileHeader);
-	for(map< string, unsigned int >::iterator mi = groupMap.begin(); mi != groupMap.end(); mi++) {
-		clef = mi -> first;
-		n = mi -> second;
-		if ( clef.compare(0, 8, "OUTGROUP") == 0 ) {
-			SequenceSelection ss = MaseTools::getSequenceSet(maseFileHeader, clef);
-			for (unsigned int i = 0; i != ss.size(); i++) {
-				psc -> setAsOutgroupMember(ss[i]);
+	const VectorSequenceContainer *seqc = NULL;
+	try {
+		seqc = ms.read( path, alpha );
+	}
+	catch (Exception & e) {
+		if (seqc != NULL)
+			delete seqc;
+		throw e;
+	}
+	PolymorphismSequenceContainer * psc = new PolymorphismSequenceContainer(* seqc);
+	Comments maseFileHeader = seqc->getGeneralComments();
+	delete seqc;
+	map<string, unsigned int> groupMap = MaseTools::getAvailableSequenceSelections(maseFileHeader);
+	for(map<string, unsigned int>::iterator mi = groupMap.begin() ; mi != groupMap.end() ; mi++) {
+		key = mi->first;
+		n = mi->second;
+		if (key.compare(0, 8, "OUTGROUP") == 0 ) {
+			SequenceSelection ss;
+			try {
+				ss = MaseTools::getSequenceSet(maseFileHeader, key);
+			}
+			catch (IOException & ioe) {
+				delete psc;
+				throw ioe;
+			}
+			for (unsigned int i = 0 ; i != ss.size() ; i++) {
+				try {
+					psc->setAsOutgroupMember(ss[i]);
+				}
+				catch (SequenceNotFoundException & snfe) {
+					delete psc;
+					throw snfe;
+				}
 			}			
 		}
 	}
-	delete seqc, sitec;
-	return(psc);
-	} catch(...) {}
+	return psc;
 }
 	 
-PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::extractIngroup (const PolymorphismSequenceContainer & psc ) throw (Exception) {
-	try {
+PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::extractIngroup (const PolymorphismSequenceContainer & psc) throw (Exception) {
 	SequenceSelection ss;	
-	PolymorphismSequenceContainer *psci = dynamic_cast<PolymorphismSequenceContainer *>(psc.clone());
-	for(unsigned int i = 0; i < psc.getNumberOfSequences(); i++) {
-		if (! psc.isIngroupMember(i) ) {
+	PolymorphismSequenceContainer * psci = dynamic_cast<PolymorphismSequenceContainer *>(psc.clone());
+	for(unsigned int i = 0; i < psc.getNumberOfSequences(); i++)
+		if (! psc.isIngroupMember(i))
 			ss.push_back(i);
-		}
+	if (ss.size() == psc.getNumberOfSequences()) {
+		delete psci;
+		throw Exception("PolymorphismSequenceContainerTools::extractIngroup: no Ingroup sequences found.");
 	}
-	/*
-	SequenceContainerTools::keepOnlySelectedSequences(
-		* dynamic_cast<const OrderedSequenceContainer *>(
-			dynamic_cast<const SiteContainer *>(psci)), ss );
-	*/
-	for(unsigned int i = ss.size() - 1; i <= 0; i--) {psci->deleteSequence(ss[i]);}
-	return( psci );
-	} catch(...) {}
+	for(unsigned int i = ss.size() - 1 ; i <= 0 ; i--)
+		psci->deleteSequence(ss[i]);
+	return psci;
 }
 
 PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::extractOutgroup(const PolymorphismSequenceContainer & psc) throw (Exception) {
 	SequenceSelection ss;
-	PolymorphismSequenceContainer *psci = dynamic_cast<PolymorphismSequenceContainer *>(psc.clone());
-	for(unsigned int i = 0; i < psc.getNumberOfSequences(); i++) {
-		if (psc.isIngroupMember(i) ) {
+	PolymorphismSequenceContainer * psci = dynamic_cast<PolymorphismSequenceContainer *>(psc.clone());
+	for(unsigned int i = 0; i < psc.getNumberOfSequences(); i++)
+		if (psc.isIngroupMember(i) )
 			ss.push_back(i);
-		}
+	if (ss.size() == psc.getNumberOfSequences()) {
+		delete psci;
+		throw Exception("PolymorphismSequenceContainerTools::extractOutgroup: no Outgroup sequences found.");
 	}
 	for(unsigned int i = ss.size() - 1; i <= 0; i--) {
 		psci->deleteSequence(ss[i]);
 	}
 	return psci;
 }
+
+PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::extractGroup(const PolymorphismSequenceContainer & psc, unsigned int group_id) throw (Exception) {
+	SequenceSelection ss;
+	PolymorphismSequenceContainer * psci = dynamic_cast<PolymorphismSequenceContainer *>(psc.clone());
+	for (unsigned int i = 0 ; i < psc.getNumberOfSequences() ; i++)
+		if (psc.getGroupId(i) == group_id)
+			ss.push_back(i);
+	if (ss.size() == 0) {
+		delete psci;
+		throw GroupNotFoundException("PolymorphismSequenceContainerTools::extractGroup: group_id not found.", group_id);
+	}
+	for (unsigned int i = ss.size() - 1 ; i <= 0 ; i--)
+		psci->deleteSequence(ss[i]);
+	return psci;
+}
 	 
-PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::getSitesWithoutGaps (const PolymorphismSequenceContainer & psc ) throw (Exception) {
-	try {
+PolymorphismSequenceContainer * PolymorphismSequenceContainerTools::getSitesWithoutGaps (const PolymorphismSequenceContainer & psc) {
 	vector<string> seqNames = psc.getSequencesNames();
-	PolymorphismSequenceContainer * noGapCont = new PolymorphismSequenceContainer( psc.getNumberOfSequences(), psc.getAlphabet() );
-	noGapCont -> setSequencesNames(seqNames, false);
+	PolymorphismSequenceContainer * noGapCont = new PolymorphismSequenceContainer(psc.getNumberOfSequences(), psc.getAlphabet());
+	noGapCont->setSequencesNames(seqNames, false);
 	unsigned int nbSeq = psc.getNumberOfSequences();
-	for (unsigned int i = 0; i < nbSeq; i++) {
-		noGapCont -> setSequenceCount( i, psc.getSequenceCount(i) );
+	for (unsigned int i = 0 ; i < nbSeq ; i++) {
+		noGapCont->setSequenceCount(i, psc.getSequenceCount(i));
 		if (! psc.isIngroupMember(i))
-			noGapCont -> setAsOutgroupMember(i);
+			noGapCont->setAsOutgroupMember(i);
 	}
 	NoGapSiteIterator ngsi(psc);
-	while(ngsi.hasMoreSites()) {
-		noGapCont -> addSite(* ngsi.nextSite());
-	}
+	while(ngsi.hasMoreSites())
+		noGapCont->addSite(* ngsi.nextSite());
 	return noGapCont;
-	}
-	catch(...) {}
 }
 
 unsigned int PolymorphismSequenceContainerTools::getNumberOfNonGapSites(const PolymorphismSequenceContainer & psc, bool ingroup) throw (Exception) {
-	try {
-	unsigned int alphasize = (psc.getAlphabet())->getSize(); 
-	unsigned int nbOfSites = psc.getNumberOfSites();
-	unsigned int count = nbOfSites;
-	unsigned int nbOfSequences = psc.getNumberOfSequences();
-	for (unsigned int j=0; j < nbOfSites; j++) {
-		const Site *si = psc.getSite(j);
-		for (unsigned int i=0; i < nbOfSequences; i++) {
-			if (ingroup) {
-				if (! psc.isIngroupMember(i)) continue;
-				if (!(si->getValue(i) < alphasize && si->getValue(i) > -1)) {
-					count--;
-					break;
-				}
-			} else {
-				if (!(si->getValue(i) < alphasize && si->getValue(i) > -1)) {
-					count--;
-					break;
-				}
-			}
+	unsigned int count = 0;
+	PolymorphismSequenceContainer * npsc = NULL;
+	SimpleSiteIterator * ssi;
+	if (ingroup) {
+		try {
+			npsc = extractIngroup(psc);
 		}
+		catch (Exception & e) {
+			if (npsc != NULL)
+				delete npsc;
+			throw e;
+		}
+		ssi = new SimpleSiteIterator(* npsc);
 	}
+	else
+		ssi = new SimpleSiteIterator(psc);
+	while (ssi->hasMoreSites())
+		if (SiteTools::hasGap(* ssi->nextSite()))
+			count++;
+	delete ssi;
 	return count;	
-	}
-	catch(...) {}
 }
--- Pop/PolymorphismSequenceContainerTools.h
+++ Pop/PolymorphismSequenceContainerTools.h
@@ -1,9 +1,9 @@
-//
-// File: PolymorphismSequenceContainerTools.h
-// Authors: bazin <bazin@univ-montp2.fr>
-//          Sylvain Gaillard <yragael2001@yahoo.fr>
-// Last modification : Tuesday July 27 2004
-//
+/*
+ * File: PolymorphismSequenceContainerTools.h
+ * Authors: bazin <bazin@univ-montp2.fr>
+ *          Sylvain Gaillard <yragael2001@yahoo.fr>
+ * Last modification : Wednesday July 28 2004
+ */
 
 #ifndef _POLYMORPHISMSEQUENCECONTAINERTOOL_H_
 #define _POLYMORPHISMSEQUENCECONTAINERTOOL_H_
@@ -14,12 +14,14 @@
 #include <Seq/MaseTools.h>
 #include <Seq/SequenceContainerTools.h>
 #include <Seq/SiteIterator.h>
+#include <Seq/SiteTools.h>
 
 // from STL
 #include <map>
 
 // From Local
 #include "PolymorphismSequenceContainer.h"
+#include "GeneralExceptions.h"
 
 using namespace std;
 
@@ -39,16 +41,14 @@ class PolymorphismSequenceContainerTools
 		 * @param path Path to the Mase+ file
 		 * @param alpha Sequence Alphabet 
 		 */        
-		static PolymorphismSequenceContainer * read(const string & path,
-				const Alphabet *alpha) throw (Exception);
+		static PolymorphismSequenceContainer * read(const string & path, const Alphabet * alpha) throw (Exception);
 
 		/**
-		 * @brief Extract ingroup sequences from a PolymorphismSequenceContainer and create a new
-		 * one.
+		 * @brief Extract ingroup sequences from a PolymorphismSequenceContainer and create a new one.
 		 *
 		 * @param psc a PolymorphismSequenceContainer reference
 		 */
-		static PolymorphismSequenceContainer * extractIngroup (const PolymorphismSequenceContainer & psc ) throw (Exception);
+		static PolymorphismSequenceContainer * extractIngroup (const PolymorphismSequenceContainer & psc) throw (Exception);
 
 		/**
 		 * @brief Extract outgroup sequences from a PolymorphismSequenceContainer and create a new one.
@@ -58,11 +58,21 @@ class PolymorphismSequenceContainerTools
 		static PolymorphismSequenceContainer * extractOutgroup (const PolymorphismSequenceContainer & psc) throw (Exception);
 
 		/**
+		 * @brief Extract a special group from the PolymorphismSequenceContainer.
+		 *
+		 * @param psc a PolymorphismSequenceContainer reference.
+		 * @param group_id the group identifier as an unsigned int.
+		 *
+		 * @throw GroupNotFoundException if group_id is not found.
+		 */
+		static PolymorphismSequenceContainer * extractGroup(const PolymorphismSequenceContainer & psc, unsigned int group_id) throw (Exception);
+
+		/**
 		 * @brief Retrieves sites without gaps from PolymorphismSequenceContainer.
 		 *
 		 * @param psc a PolymorphismSequenceContainer reference
 		 */
-		static PolymorphismSequenceContainer * getSitesWithoutGaps (const PolymorphismSequenceContainer & psc ) throw (Exception);
+		static PolymorphismSequenceContainer * getSitesWithoutGaps (const PolymorphismSequenceContainer & psc);
 
 		/**
 		 * @brief Return number of sites without gaps in a PolymorphismSequenceContainer.
