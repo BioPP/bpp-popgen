0f8cfc243fa1c021d29583c8f6408d10c8fce457
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2004-07-29 07:30:14 +0000

    Everything is GPL ;-)

--- Pop/GeneralExceptions.h
+++ Pop/GeneralExceptions.h
@@ -1,7 +1,26 @@
 /*
  * File GeneralExceptions.h
  * Author : Sylvain Gaillard <yragael2001@yahoo.fr>
- * Last modification : Tuesday July 06 2004
+ * Last modification : Thursday July 29 2004
+ *
+ * Copyright (C) 2004 Sylvain Gaillard and the
+ *                    PopLib Development Core Team
+ *
+ * This file is part of PopLib.
+ *
+ * PopLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PopLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PopLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 // Secured inclusion of header's file
--- Pop/PolymorphismSequenceContainer.h
+++ Pop/PolymorphismSequenceContainer.h
@@ -1,8 +1,27 @@
 /*
  * File: PolymorphismSequenceContainer.h
- * Authors: bazin <bazin@univ-montp2.fr>
+ * Authors: Eric Bazin <bazin@univ-montp2.fr>
  *          Sylvain Gaillard <yragael2001@yahoo.fr>
- * Last modification : Wednesday July 28 2004
+ * Last modification : Thursday July 29 2004
+ *
+ * Copyright (C) 2004 Eric Bazin, Sylvain Gaillard and the
+ *                    PopLib Development Core Team
+ *
+ * This file is part of PopLib.
+ *
+ * PopLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PopLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PopLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef _POLYMORPHISMSEQUENCECONTAINER_H_
@@ -230,7 +249,7 @@ class PolymorphismSequenceContainer : public VectorSiteContainer
 		/**
 		 * @brief Get the count of a sequence by name.
 		 *
-		 * @brief throw SequenceNotFoundException if name is not found among the sequences' names.
+		 * @throw SequenceNotFoundException if name is not found among the sequences' names.
 		 */
 		unsigned int getSequenceCount(const string &name) const throw (SequenceNotFoundException);
 		
