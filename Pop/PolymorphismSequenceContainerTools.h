12660a3ed3f24adc78d90acd8789a950dd282635
glemin <glemin@98694c10-1272-4568-8599-3ebed480974b>
2004-10-11 09:42:32 +0000

    Sylvain Glémin Ajout de méthodes: excludeFalnkingRegion getCodingSite getNonCodingSite getOnePosition getIntrons get5Prime get3Prime

--- Pop/PolymorphismSequenceContainerTools.h
+++ Pop/PolymorphismSequenceContainerTools.h
@@ -46,7 +46,7 @@ using namespace std;
 
 class PolymorphismSequenceContainerTools
 {
-	public: 
+	public:
 		// Class destructor:
 		~PolymorphismSequenceContainerTools();
 
@@ -58,8 +58,8 @@ class PolymorphismSequenceContainerTools
 		 * when selection tag begin with OUTGROUP (see Polymorphix)
 		 *
 		 * @param path Path to the Mase+ file
-		 * @param alpha Sequence Alphabet 
-		 */        
+		 * @param alpha Sequence Alphabet
+		 */
 		static PolymorphismSequenceContainer * read(const string & path, const Alphabet * alpha) throw (Exception);
 
 		/**
@@ -95,7 +95,7 @@ class PolymorphismSequenceContainerTools
 
 		/**
 		 * @brief Return number of sites without gaps in a PolymorphismSequenceContainer.
-		 * 
+		 *
 		 * @param psc a PolymorphismSequenceContainer reference
 		 * @param ingroup a boolean set to true if you want to take only ingroup sequences into account
 		 */
@@ -110,6 +110,71 @@ class PolymorphismSequenceContainerTools
 		 */
 		static unsigned int getNumberOfCompleteSites(const PolymorphismSequenceContainer & psc, bool ingroup) throw (Exception);
 
+
+		/**
+		 * @brief Retrieves complete sites from a PolymorphismSequenceContainer.
+		 *
+		 * @param psc a PolymorphismSequenceContainer reference
+		 */
+		static PolymorphismSequenceContainer * getCompleteSites(const PolymorphismSequenceContainer & psc);
+
+
+		 /**
+          * @brief exclude flanking sites with gap but keep gap sites within the alignment
+	      *
+	      * @param psc a PolymorphismSequenceContainer reference
+	      */
+		static PolymorphismSequenceContainer * excludeFlankingGap(const PolymorphismSequenceContainer & psc) throw (Exception);
+
+		/**
+	     * @brief Retrieve coding sites defined in the mase file header
+	     *
+	     * @param psc a PolymorphismSequenceContainer reference
+	     * Be carefull: to use before excluding gap
+	     */
+		static PolymorphismSequenceContainer * getCodingSites(const PolymorphismSequenceContainer & psc) throw (Exception);
+
+	    /**
+	     * @brief Retrieve non-coding sites defined in the mase file header
+	     *
+	     * @param psc a PolymorphismSequenceContainer reference
+	     * Be carefull: to use before excluding gap
+	     */
+		static PolymorphismSequenceContainer * getNonCodingSites(const PolymorphismSequenceContainer & psc) throw (Exception);
+
+	    /**
+	     * @brief Retrieve sites at one codon position (1,2,3)
+	     *
+	     * @param psc a PolymorphismSequenceContainer reference  *
+	     * Be carefull: to use before excluding gap
+	     * The first position of the alignement is supposed to be 1 (to be checked before using this method)
+	     */
+	    static PolymorphismSequenceContainer * getOnePosition(const PolymorphismSequenceContainer & psc, unsigned int pos) throw (Exception);
+
+	    /**
+	     * @brief Retrieve intron sites
+	     *
+	     * Same as getNonCodgingSites but exclude 5' and 3' falnking regions if there are
+             * Assumed that the first coding site correspond to the first position
+	     * @param psc a PolymorphismSequenceContainer
+	     */
+             static PolymorphismSequenceContainer * getIntrons(const PolymorphismSequenceContainer & psc) throw (Exception);
+
+	    /**
+	     * @brief Retrieve 5' sites
+	     *
+             * Assumed that the first coding site correspond to the first position
+	     * @param psc a PolymorphismSequenceContainer
+	     */
+             static PolymorphismSequenceContainer * get5Prime(const PolymorphismSequenceContainer & psc) throw (Exception);
+
+	    /**
+	     * @brief Retrieve 3' sites
+	     *
+             * Assumed that the first coding site correspond to the first position
+	     * @param psc a PolymorphismSequenceContainer
+	     */
+             static PolymorphismSequenceContainer * get3Prime(const PolymorphismSequenceContainer & psc) throw (Exception);
 		/*******************************************************************************/
 };
 #endif // _POLYMORPHISMSEQUENCECONTAINERTOOL_H_
