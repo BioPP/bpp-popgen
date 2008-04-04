a85dc5256993967f03d051f324416ea24130be91
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2008-04-04 12:52:00 +0000

    Cleaned from debug/devel output informations.

--- Pop/GeneMapperCsvExport.cpp
+++ Pop/GeneMapperCsvExport.cpp
@@ -59,40 +59,11 @@ void GeneMapperCsvExport::read(istream & is, DataSet & data_set) throw (Exceptio
 {
   if (!is)
     throw IOException("GeneMapperCsvExport::read: fail to open stream.");
-  bool debug = true;
-  
-  if (debug)
-    cerr << "--- In GeneMapperCsvExport ---" << endl;
 
   /*
    * Feed a DataTable with the data
    */
-
-  // Strange that's not working
   DataTable dt = * DataTable::read(is, "\t", true, -1);
-
-  /*
-  // So I do it by myself
-  // Get the number of columns
-  string line = FileTools::getNextLine(is);
-  line = TextTools::removeLastNewLines(line); //Cleaning for garbage if file came from Micro$
-  StringTokenizer st(line, "\t", true);
-  // Create the DataTable according to the number of columns
-  DataTable dt(st.numberOfRemainingTokens());
-  // Set the columns' names
-  vector<string> row1(st.getTokens().begin(), st.getTokens().end());
-  dt.setColumnNames(row1);
-  //Feed the DataTable with remaining rows
-  while (!is.eof()) {
-    line = FileTools::getNextLine(is);
-    line = TextTools::removeLastNewLines(line);
-    if (TextTools::isEmpty(line))
-      continue;
-    StringTokenizer st (line, "\t", true);
-    vector<string> row2(st.getTokens().begin(), st.getTokens().end());
-    dt.addRow(row2);
-  }
-  */
 
   /*
    * Fixe the individuals' name if there is duplicate in the file
@@ -122,20 +93,6 @@ void GeneMapperCsvExport::read(istream & is, DataSet & data_set) throw (Exceptio
   markers= VectorTools::unique(markers);
   unsigned int loc_nbr = markers.size();
 
-  if (debug) {
-    cerr << "DataTable R x C : " << dt.getNumberOfRows() << " x " << dt.getNumberOfColumns() << endl;
-    cerr << "Sample Name nbr : " << ind_names.size() << endl;
-    cerr << "Marker nbr      : " << loc_nbr << endl;
-    //for (unsigned int i = 0 ; i < ind_names.size() ; i++) {
-    //  cerr << ind_names[i] << "[" << ind_count[ind_names[i]] << "] ";
-    //}
-    //cerr << endl;
-
-    //for (unsigned int i = 0 ; i < dt.getNumberOfRows() ; i++) {
-    //  cerr << dt.getRow(i).size() << endl;
-    //}
-  }
-
   /*
    * Loci number
    */
@@ -164,8 +121,6 @@ void GeneMapperCsvExport::read(istream & is, DataSet & data_set) throw (Exceptio
     al.setLocusInfo(i, LocusInfo(markers[i], LocusInfo::UNKNOWN));
     vector<unsigned int> v = VectorTools::whichAll(dt.getColumn("Marker"), markers[i]);
     alleles_pos.push_back(v);
-    if (debug)
-      cerr << "marker " << markers[i] << " : " << v.size() << endl;
   }
   for (unsigned int i = 0 ; i < alleles_cols.size() ; i++) {
     for (unsigned int j = 0 ; j < markers.size() ; j++) {
@@ -175,10 +130,6 @@ void GeneMapperCsvExport::read(istream & is, DataSet & data_set) throw (Exceptio
           m_allele.push_back(dt(alleles_pos[j][k],alleles_cols[i]));
       }
       m_allele = VectorTools::unique(m_allele);
-      //if (debug) {
-      //  cerr << markers[j] << "\t" << m_allele.size() << "\t";
-      //  VectorTools::print(m_allele, cerr);
-      //}
       if (m_allele.size() > 0)
         al.addAlleleInfoByLocusName(markers[j], BasicAlleleInfo(m_allele[0]));
     }
@@ -194,55 +145,17 @@ void GeneMapperCsvExport::read(istream & is, DataSet & data_set) throw (Exceptio
     vector<unsigned int> alleles;
     for (unsigned int j = 0 ; j < alleles_cols.size() ; j++) {
       if (!TextTools::isEmpty(dt(i,alleles_cols[j]))) {
-        //cerr << dt(i, mark_col_index) << " " << (data_set.getLocusInfoByName(dt(i, mark_col_index)))->getName() << " " << (data_set.getLocusInfoByName(dt(i,mark_col_index)))->getNumberOfAlleles() << endl;
         unsigned int num = (data_set.getLocusInfoByName(dt(i, mark_col_index)))->getAlleleInfoKey(dt(i,alleles_cols[j]));
         alleles.push_back(num);
       }
     }
     alleles = VectorTools::unique(alleles);
     MultiAlleleMonolocusGenotype ma(alleles);
-    cerr << dt(i, ind_col_index) << " " << dt(i, mark_col_index) << " : ";
-    VectorTools::print(alleles, cerr);
     if (!data_set.getIndividualByIdFromGroup(0,dt(i, ind_col_index))->hasGenotype())
       data_set.initIndividualGenotypeInGroup(0, data_set.getIndividualPositionInGroup(0,dt(i, ind_col_index)));
     if (alleles.size())
       data_set.setIndividualMonolocusGenotypeInGroup(0, data_set.getIndividualPositionInGroup(0, dt(i, ind_col_index)), data_set.getAnalyzedLoci()->getLocusInfoPosition(dt(i, mark_col_index)),ma);
   }
-
-  /*
-  // Groups
-  for (unsigned int i = 0 ; i < grp_nbr ; i++) {
-    data_set.addEmptyGroup(i);
-    // Group name ... Now used khalid
-    temp = FileTools::getNextLine(is);
-    data_set.setGroupName(i, temp);
-
-    // Number of individuals
-    unsigned int ind_nbr;
-    temp = FileTools::getNextLine(is);
-    stringstream tmp(temp);
-    tmp >> ind_nbr;
-    for (unsigned int j = 0 ; j < ind_nbr ; j++) {
-      temp = FileTools::getNextLine(is);
-      string ind_name(temp.begin(), temp.begin()+11);
-      temp = string(temp.begin()+11, temp.end());
-      data_set.addEmptyIndividualToGroup(i, TextTools::removeSurroundingWhiteSpaces(ind_name) + string("_") + TextTools::toString(i+1) + string("_") + TextTools::toString(j+1));
-      data_set.initIndividualGenotypeInGroup(i, j);
-      StringTokenizer alleles(temp, string(" "));
-      //cout << alleles.numberOfRemainingTokens() << endl;
-      for (unsigned int k = 0 ; k < loc_nbr ; k++) {
-        string tmp_string = alleles.nextToken();
-        vector<string> tmp_alleles;
-        tmp_alleles.push_back(string(tmp_string.begin(), tmp_string.begin() + 3));
-        tmp_alleles.push_back(string(tmp_string.begin() + 3, tmp_string.begin() + 6));
-        if (tmp_alleles[0] != string("000") && tmp_alleles[1] != string("000"))
-          data_set.setIndividualMonolocusGenotypeByAlleleIdInGroup(i, j, k, tmp_alleles);
-      }
-    }
-  }
-  */
-  if (debug)
-    cerr << "--- Out GeneMapperCsvExport ---" << endl;
 }
 
 void GeneMapperCsvExport::read(const string & path, DataSet & data_set) throw (Exception)
