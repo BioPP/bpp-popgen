0cbaba6d6ba91e431ed0b047f6deeb1c91e78715
sylvain <sylvain@98694c10-1272-4568-8599-3ebed480974b>
2008-04-09 12:58:20 +0000

    Added DarwinDon and DarwinVarSingle output class.

---
+++ Pop/DarwinDon.cpp
@@ -0,0 +1,84 @@
+//
+// File DarwinDon.cpp
+// Authors : Sylvain Gaillard
+// Last modification : April 7, 2008
+//
+
+/*
+   Copyright or © or Copr. CNRS, (April 7, 2008)
+
+   This software is a computer program whose purpose is to provide classes
+   for population genetics analysis.
+
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use, 
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info". 
+
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability. 
+
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or 
+   data to be ensured and,  more generally, to use and operate it in the 
+   same conditions as regards security. 
+
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+   */
+
+#include "DarwinDon.h"
+
+using namespace bpp;
+
+DarwinDon::DarwinDon() {}
+
+DarwinDon::~DarwinDon() {}
+
+const string DarwinDon::getFormatName()
+{
+  return "Darwin .don";
+}
+
+const string DarwinDon::getFormatDescription()
+{
+  return "Darwin .don file store data identifying individuals.";
+}
+
+void DarwinDon::write(ostream & os, const DataSet & data_set) const throw (Exception)
+{
+  if (!os)
+    throw IOException("DarwinDon::write: fail to open stream.");
+  os << "@DARwin 5.0 - DON" << endl;
+  unsigned int ind_nbr = 0;
+  for (unsigned int i = 0 ; i < data_set.getNumberOfGroups() ; i++)
+    ind_nbr += data_set.getNumberOfIndividualsInGroup(i);
+  vector<string> header;
+  header.push_back("N°");
+  header.push_back("Name");
+  os << ind_nbr << "\t" << header.size() - 1 << endl;
+  VectorTools::print(header, os, "\t");
+  unsigned int ind_index = 0;
+  for (unsigned int i = 0 ; i < data_set.getNumberOfGroups() ; i++) {
+    unsigned int ind_nbr_ig = data_set.getNumberOfIndividualsInGroup(i);
+    for (unsigned int j = 0 ; j < ind_nbr_ig ; j++) {
+      os << j + (i * ind_nbr_ig) + 1 << "\t" << data_set.getIndividualAtPositionFromGroup(i, j)->getId() << endl;
+    }
+  }
+}
+
+void DarwinDon::write(const string & path, const DataSet & data_set, bool overwrite) const throw (Exception)
+{
+  AbstractODataSet::write(path, data_set, overwrite);
+}
+
---
+++ Pop/DarwinVarSingle.h
@@ -0,0 +1,91 @@
+//
+// File DarwinVarSingle.h
+// Author : Sylvain Gaillard
+// Last modification : April 7, 2008
+//
+
+/*
+   Copyright or © or Copr. CNRS, (April 7, 2008)
+
+   This software is a computer program whose purpose is to provide classes
+   for population genetics analysis.
+
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use, 
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info". 
+
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability. 
+
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or 
+   data to be ensured and,  more generally, to use and operate it in the 
+   same conditions as regards security. 
+
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+   */
+
+#ifndef _DARWIN_VAR_SINGLE_H_
+#define _DARWIN_VAR_SINGLE_H_
+
+// From Utils
+#include <Utils/TextTools.h>
+#include <Utils/FileTools.h>
+#include <Utils/Exceptions.h>
+#include <Utils/StringTokenizer.h>
+
+// From local Pop
+#include "AbstractODataSet.h"
+
+namespace bpp
+{
+
+  /**
+   * @brief The Darwin .don output format for popgenlib.
+   */
+  class DarwinVarSingle:
+    public AbstractODataSet
+  {
+
+    public: // Constructor and destructor
+      DarwinVarSingle();
+      ~DarwinVarSingle();
+
+    public:
+      /**
+       * @name The ODataSet interface.
+       * @{
+       */
+      void write(ostream & os, const DataSet & data_set) const throw (Exception);
+      void write(const string & path, const DataSet & data_set, bool overwrite) const throw (Exception);
+      /**
+       * @}
+       */
+
+      /**
+       * @name The IODataSet interface
+       * @{
+       */
+      virtual const string getFormatName();
+      virtual const string getFormatDescription();
+      /**
+       * @}
+       */
+  };
+
+} //end of namespace bpp;
+
+#endif // _DARWIN_VAR_SINGLE_H_
+
